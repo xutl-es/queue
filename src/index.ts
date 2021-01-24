aa72b610eea671a3350a26689177377710e04dcb
XUTL <dev@xutl.es>
2021-01-24 15:54:19 +0000

    add merge function

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,23 @@ export { ThrottledQueue, ThrottledInterface, IterableThrottledInterface };
 export const queue = IterableQueue.create;
 export const throttled = ThrottledQueue.create;
 
+export function merge<T>(items: AsyncIterable<T>[]): AsyncIterable<T> {
+	const result = queue<T>();
+	const drain = async (iter: AsyncIterable<T>) => {
+		for await (const value of iter) result.push(value);
+	};
+	Promise.all(items.map(drain)).then(
+		() => result.done(),
+		(err: Error) => result.error(err),
+	);
+	return result;
+}
+
 const Default = Object.freeze({
 	IterableQueue,
 	queue,
 	ThrottledQueue,
 	throttled,
+	merge,
 });
 export default Default;
